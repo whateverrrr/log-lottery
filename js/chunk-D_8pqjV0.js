91d7e49f242892952a675e2e7730a06733fcef27
whateverrrr <whateverrrr@users.noreply.github.com>
2026-02-02 11:37:45 +0000

    deploy: 0ffc18b349b5b4a544b1ea8888a9e99b2ff25d35

---
+++ js/chunk-D_8pqjV0.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-Dsxdx0na.js";import{o,K as s,J as t}from"./chunk-BQtGIBGZ.js";const n=r({},[["render",function(r,n){const a=t("router-view");return o(),s(a)}]]);export{n as default};
